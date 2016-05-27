e3bf161d8ec41e5c16623d98c518c8e55cc8c476
binku <binku87@gmail.com>
2016-05-27 11:53:05 +0800

    Real link and text in bar

--- action_bar.go
+++ action_bar.go
@@ -3,6 +3,7 @@ package action_bar
 import (
 	"bytes"
 	"github.com/qor/admin"
+	"github.com/qor/qor"
 	"html/template"
 	"net/http"
 	"os"
@@ -12,27 +13,46 @@ import (
 )
 
 type ActionBar struct {
+	auth  admin.Auth
+	admin *admin.Admin
 }
 
 func (bar *ActionBar) Register(admin *admin.Admin) {
+	bar.admin = admin
 	router := admin.GetRouter()
 	router.Get("/switch_mode", SwitchMode)
 }
 
-func (bar *ActionBar) RenderIncludedTag(Request *http.Request) template.HTML {
+func (bar *ActionBar) SetAuth(auth admin.Auth) {
+	bar.auth = auth
+}
+
+func (bar *ActionBar) RenderIncludedTag(w http.ResponseWriter, r *http.Request) template.HTML {
 	var file string
 	for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
 		file = path.Join(gopath, "src/github.com/qor/action_bar/views/themes/action_bar/assets/action_bar.tmpl")
 	}
 
 	var checked bool
-	if cookie, err := Request.Cookie("qor-action-bar"); err == nil {
+	if cookie, err := r.Cookie("qor-action-bar"); err == nil {
 		checked = cookie.Value == "true"
 	}
 
 	var result = bytes.NewBufferString("")
+	context := bar.admin.NewContext(w, r)
 	if tmpl, err := template.New(filepath.Base(file)).ParseFiles(file); err == nil {
-		if err = tmpl.Execute(result, struct{ Checked bool }{Checked: checked}); err == nil {
+		context := struct {
+			Checked     bool
+			Auth        admin.Auth
+			Context     *admin.Context
+			CurrentUser qor.CurrentUser
+		}{
+			Checked:     checked,
+			Auth:        bar.auth,
+			Context:     context,
+			CurrentUser: bar.auth.GetCurrentUser(context),
+		}
+		if err = tmpl.Execute(result, context); err == nil {
 			return template.HTML(result.String())
 		}
 	}
