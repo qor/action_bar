f3ef4a9d1010c8d6e0316bf633954af2d6d0c480
Jinzhu <wosmvp@gmail.com>
2016-06-01 18:38:36 +0800

    Redirect back to referrer url

--- controller.go
+++ controller.go
@@ -1,12 +1,19 @@
 package action_bar
 
 import (
-	"github.com/qor/admin"
 	"net/http"
+
+	"github.com/qor/admin"
 )
 
 func SwitchMode(context *admin.Context) {
 	cookie := http.Cookie{Name: "qor-action-bar", Value: context.Request.URL.Query().Get("checked"), Path: "/", HttpOnly: true}
 	http.SetCookie(context.Writer, &cookie)
-	http.Redirect(context.Writer, context.Request, "/", http.StatusFound)
+
+	referrer := context.Request.Referer()
+	if referrer == "" {
+		referrer = "/"
+	}
+
+	http.Redirect(context.Writer, context.Request, referrer, http.StatusFound)
 }
